edfced32fb563b1727c22ae1edb485fcafa55cb6
Gabirel77 <gabriel.sm@estudante.iftm.edu.br>
2026-01-29 18:14:11 -0300

    Sistema em python que transforma arquivos tif em png com analise de dados

---
+++ main.py
@@ -0,0 +1,12 @@
+import rasterio
+import matplotlib.pyplot as plt
+import numpy as np
+
+with rasterio.open("NDVI_small_area.tif") as src:
+    ndvi = src.read(1)
+
+plt.imshow(ndvi, cmap="RdYlGn")
+plt.colorbar(label="NDVI")
+plt.title("NDVI - Sentinel-2")
+plt.show()
+plt.savefig("ndvi.png", dpi=300)
